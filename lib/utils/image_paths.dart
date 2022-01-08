552e6404d3184ddf6e5e025ebfa804ec0f0e99c9
dayvedev <dayvedev@gmail.com>
2022-01-08 00:05:29 +0000

    Create image_paths.dart

---
+++ lib/utils/image_paths.dart
@@ -0,0 +1,38 @@
+
+class ImagePaths {
+
+  static final logo = 'assets/images/help_desk.svg';
+  static final icLauncherLogo = 'assets/images/ic_launcher_logo.png';
+
+  //Intro Page
+  static const introBg = 'assets/images/intro.png';
+  static const intro = 'assets/images/intro.png';
+
+  static final coin = 'assets/images/coin.jpeg';
+  static final cylinder = 'assets/images/cylinder.jpeg';
+
+  static final imgMoveIn = 'assets/images/move_in.png';
+  static final imgAgree = 'assets/images/agree.png';
+
+  // Login Page
+  static final loginImg = 'assets/images/login.svg';
+
+  // SignUp Page
+  static final signUpImg = 'assets/images/signup.png';
+
+  // HomeNav Page
+  static final property = 'assets/images/logo.png';
+  static final construction = 'assets/images/construction.png';
+  static final propPlaceholder = 'assets/images/prop_placeholder.png';
+
+  //Profile Page
+  static final profile = 'assets/images/profile.png';
+
+  //AllApplicationsPage
+  static final buyHouse = 'assets/images/buy_house.png';
+
+  //PaymentsPage
+  static final transactions = 'assets/images/transactions.png';
+
+
+}
