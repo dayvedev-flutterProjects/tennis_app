efd77b5be63bf6e94937f457738e6be225951d57
dayvedev <dayvedev@gmail.com>
2022-01-08 00:05:24 +0000

    Create intro_page.dart

---
+++ lib/ui/pages/intro_page.dart
@@ -0,0 +1,60 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:tennis_app/utils/image_paths.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //padding: EdgeInsets.only(left: 50, right: 50, bottom: 60),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: const ExactAssetImage(ImagePaths.introBg),
+          fit: BoxFit.fill,
+          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.01), BlendMode.darken),
+        ),
+      ),
+
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20,),
+
+              Center(
+                child: SizedBox(
+                  width: 250.0,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                      fontSize: 30.0, fontWeight: FontWeight.bold
+                    ),
+                    child: AnimatedTextKit(
+                      isRepeatingAnimation: false,
+
+                      animatedTexts: [
+                        TypewriterAnimatedText('SPORTVOLO'),
+                      ],
+                      onFinished: () {
+                        print("Tap Event");
+                      },
+                      onTap: () {
+                        print("Tap Event");
+                      },
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
