6ac096b94d265a8716f9ab02e0dd361ad0373f02
dayvedev <dayvedev@gmail.com>
2022-02-22 11:50:45 +0000

    feat: create AppStockData class

---
+++ lib/models/app_stock_data.dart
@@ -0,0 +1,14 @@
+
+class AppStockData{
+
+  static const List<String> tournamentEvents = [
+    'All Events',
+    'Men\'s Singles', 'Women\'s Singles', 'Men\'s Doubles' ,'Women\'s Doubles','Mixed Doubles',
+    'Junior Boys\' Singles', 'Junior Girls\' Singles', 'Junior Boys\' Doubles' ,'Junior Boys\' Doubles',
+  ];
+
+  static const List<String> tournamentRounds = [
+    'R1', 'R2','R3', 'R4','QF','SF', 'F',
+  ];
+
+}
