23000eefb914ca21f386de132c06f1d3922fe450
dayvedev <dayvedev@gmail.com>
2022-02-26 18:13:47 +0000

    Create tournament.dart

---
+++ lib/models/tournament.dart
@@ -0,0 +1,13 @@
+
+class Tournament{
+
+  String countryCode;
+  String tournamentTitle;
+  String tournamentCategory;
+  String matchesPlayed;
+  String totalMatches;
+
+  Tournament({required this.countryCode, required this.tournamentTitle, required this.tournamentCategory,
+    required this.matchesPlayed, required this.totalMatches});
+
+}
