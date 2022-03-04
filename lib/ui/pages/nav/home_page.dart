88cabcd889d271f64c174f93e12652f9cd4cf0f9
dayvedev <dayvedev@gmail.com>
2022-03-04 12:29:46 +0000

    Update home_page.dart

--- lib/ui/pages/nav/home_page.dart
+++ lib/ui/pages/nav/home_page.dart
@@ -3,8 +3,12 @@ import 'package:flag/flag_enum.dart';
 import 'package:flag/flag_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:tennis_app/models/app_stock_data.dart';
+import 'package:tennis_app/models/top_matches.dart';
+import 'package:tennis_app/models/tournament.dart';
 import 'package:tennis_app/ui/widgets/molecules/matches_card.dart';
 import 'package:tennis_app/utils/colors.dart';
 import 'package:tennis_app/utils/image_paths.dart';
@@ -32,6 +36,11 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light,
+    ));
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       body: SafeArea(
@@ -68,16 +77,21 @@ class _HomePageState extends State<HomePage> {
 
           CarouselSlider.builder(
 
-              itemCount: 3,
-              itemBuilder: (context, itemIndex, r) =>
-              const MatchesCard(
-                tournament: "Australian Open",
-                tennisAssociation: "ATP",
-                round: "1st Round",
-                time: "19:00",
-                player1: "R. Nadal",
-                player2: "D. Medvedev",
-              ),
+              itemCount: AppStockData.topMatches.length,
+              itemBuilder: (context, itemIndex, r) {
+                TopMatches topMatch = AppStockData.topMatches[itemIndex];
+              return MatchesCard(
+                tournament: topMatch.tournament,
+                tennisAssociation: topMatch.tournamentCategory,
+                round: topMatch.round,
+                time: topMatch.time,
+                player1: topMatch.player1Name,
+                player2: topMatch.player2Name,
+                chipLabel: topMatch.playStatus,
+                chipIcon: Icons.check_circle,
+                chipIconColor:topMatch.playStatus.toLowerCase() == "live"? null :Colors.green,
+              );
+              },
             options: CarouselOptions(
               height: 200,
               viewportFraction: 0.9,
@@ -97,7 +111,7 @@ class _HomePageState extends State<HomePage> {
               Center(
                 child: AnimatedSmoothIndicator(
                     activeIndex: _current,
-                    count: 3,
+                    count: 4,
                   effect: const WormEffect(
                     activeDotColor: AppColors.colorPrimaryDark,
                     dotHeight: 8
@@ -137,191 +151,42 @@ class _HomePageState extends State<HomePage> {
 
               const SizedBox(height: 5,),
 
-              Card(
-                margin: const EdgeInsets.symmetric(horizontal: 12),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: ListTile(
-                  leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      //child: Flag.fromCode(FlagsCode.AU, height: 46, width: 46)),
-                      child: Flag.fromString("AU", height: 46, width: 46)),
-                  title: Text("Australia Open"),
-                  subtitle: Text("ATP"),
-                  trailing: SizedBox(
-                    width: 50,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text("1", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.red),),
-                          Text("17"),
-                        ],
-                      )
-                  ),
-                ),
-              ),
-              const SizedBox(height: 15,),
+              ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: AppStockData.tournaments.length,
+                  itemBuilder: (BuildContext context, int index){
+                    Tournament tournament = AppStockData.tournaments[index];
+                    return Card(
+                      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 8.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: ListTile(
+                        leading: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Flag.fromString(tournament.countryCode, height: 46, width: 46)),
+                        title: Text(tournament.tournamentTitle),
+                        subtitle: Text(tournament.tournamentCategory),
+                        trailing: SizedBox(
+                            width: 50,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(tournament.matchesPlayed, style: TextStyle( fontWeight: FontWeight.bold, color: Colors.red),),
+                                Text(tournament.totalMatches),
 
-              Card(
-                margin: EdgeInsets.symmetric(horizontal: 12),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: ListTile(
-                  leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: Flag.fromCode(FlagsCode.AU, height: 46, width: 46)),
-                  title: Text("Australia Open"),
-                  subtitle: Text("ATP"),
-                  trailing: SizedBox(
-                      width: 50,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text("1", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.red),),
-                          Text("17"),
-                        ],
-                      )
-                  ),
-                  onTap: ()=>Navigator.pushNamed(context, Routes.tournamentDetailsPage),
-                ),
+                              ],
+                            )
+                        ),
+                        onTap: ()=>Navigator.pushNamed(context, Routes.tournamentDetailsPage),
+                      ),
+                    );
+                  }
               ),
 
               const SizedBox(height: 15,),
 
-              Container(
-                //height: 200,
-                margin: const EdgeInsets.symmetric(horizontal: 12.0),
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  //borderRadius: BorderRadius.circular(17),
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                         RichText(
-                           text: TextSpan(
-                             text: '1st Round  \u{FF65} ',
-                             style: const TextStyle(fontSize: 12, color: Colors.black),
-                             children: [
-
-                               TextSpan(
-                                 text: " Court 7 ",
-                               ),
-
-                               // TextSpan(
-                               //   text: "$round \u{FF65} ",
-                               // ),
-                               //
-                               // const WidgetSpan(
-                               //   child: Icon(Icons.access_time, size: 16, color: Colors.white,),
-                               // ),
-                               // TextSpan(
-                               //   text: time,
-                               // ),
-                             ],
-
-                           ),
-                         ),
-
-                         RichText(
-                           textAlign: TextAlign.end,
-                           text: TextSpan(
-                             //text: '1st Round \u{FF65} ',
-                             style: const TextStyle(fontSize: 12,
-                                 color: Colors.black,
-
-                             ),
-                             children: [
-                               const WidgetSpan(
-                                 child: Padding(
-                                   padding: EdgeInsets.only(bottom: 3.0),
-                                   child: Icon(Icons.circle, size: 8, color: Colors.black,),
-                                 ),
-                               ),
-
-                               TextSpan(
-                                 text: " Live ",
-                               ),
-
-                               // TextSpan(
-                               //   text: "$round \u{FF65} ",
-                               // ),
-                               //
-                               // const WidgetSpan(
-                               //   child: Icon(Icons.access_time, size: 16, color: Colors.white,),
-                               // ),
-                               // TextSpan(
-                               //   text: time,
-                               // ),
-                             ],
-
-                           ),
-                         ),
-
-                       ],
-                      ),
-                    ),
-
-                    Divider(),
-
-                    ListTile(
-                      dense:true,
-
-                      visualDensity: VisualDensity(horizontal: -4, vertical: -4),
-                      contentPadding: EdgeInsets.only(left: 0.0, right: 0.0),
-                      leading: CircleAvatar(
-                          child: Icon(Icons.person_outline),
-                      ),
-                      title: Text("Australia Open"),
-                      // subtitle: Text("ATP"),
-                      trailing: SizedBox(
-                          width: 50,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text("1", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.red),),
-                              Text("17"),
-                            ],
-                          )
-                      ),
-                    ),
-
-                    Divider(),
-
-                    ListTile(
-                      dense:true,
-
-                      visualDensity: VisualDensity(horizontal: -4, vertical: -4),
-                      contentPadding: EdgeInsets.only(left: 0.0, right: 0.0),
-                      leading: CircleAvatar(
-                        child: Icon(Icons.person_outline),
-                      ),
-                      title: Text("Australia Open"),
-                      // subtitle: Text("ATP"),
-                      trailing: SizedBox(
-                          width: 50,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text("1", style: TextStyle( fontWeight: FontWeight.bold, color: Colors.red),),
-                              Text("17"),
-                            ],
-                          )
-                      ),
-                    ),
-                  ],
-                ),
-              ),
 
 
 
