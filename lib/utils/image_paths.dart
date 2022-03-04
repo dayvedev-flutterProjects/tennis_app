577b9c23dcc3caa295ee02b6bfd9d56be907578f
dayvedev <dayvedev@gmail.com>
2022-03-04 12:30:02 +0000

    Update image_paths.dart

--- lib/utils/image_paths.dart
+++ lib/utils/image_paths.dart
@@ -39,4 +39,18 @@ class ImagePaths {
   static const String dMedvedev= 'https://i.goalzz.com/?i=o%2Fn%2F6%2F14%2Fdaniil-medvedev-1.png';
   static const String rFederer= 'https://www.tennisworldusa.org/world_cms/images/tennis_players/player104.png';
 
+  static const List<String> player2s = [
+    "assets/images/avatars/medvedev.png",
+    "assets/images/avatars/d_collins.jpg",
+    "assets/images/avatars/tsitsipas.png",
+    "assets/images/avatars/osaka.jpeg",
+
+  ];
+  static const List<String> player1s = [
+    "assets/images/avatars/nadal.png",
+    "assets/images/avatars/a_barty.jpeg",
+    "assets/images/avatars/federer.png",
+    "assets/images/avatars/serena.png",
+
+  ];
 }
