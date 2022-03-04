c43b9f01c3e277b8f6eac0915781766440377c6d
dayvedev <dayvedev@gmail.com>
2022-03-04 12:29:31 +0000

    Create top_matches.dart

---
+++ lib/models/top_matches.dart
@@ -0,0 +1,16 @@
+
+class TopMatches{
+  final String tournament;
+  final String tournamentCategory;
+  final String round;
+  final String time;
+  final String playStatus;
+  final String player1Name;
+  final String player2Name;
+
+  TopMatches({required this.tournament, required this.tournamentCategory, required this.round, required this.time,
+      required this.playStatus, required this.player1Name, required this.player2Name});
+
+
+
+}
