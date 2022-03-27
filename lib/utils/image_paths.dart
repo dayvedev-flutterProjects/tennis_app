b8c45ba616b0922e41a6c5dd9f4a4f67d2ed63ca
dayvedev <dayvedev@gmail.com>
2022-03-27 21:00:40 +0000

    Update image_paths.dart

--- lib/utils/image_paths.dart
+++ lib/utils/image_paths.dart
@@ -1,39 +1,13 @@
 
 class ImagePaths {
 
-  static final logo = 'assets/images/help_desk.svg';
-  static final icLauncherLogo = 'assets/images/ic_launcher_logo.png';
-
   //Intro Page
   static const introBg = 'assets/images/intro.png';
   static const intro = 'assets/images/intro.png';
 
-  static final topMatchCard = 'assets/images/top_matches_card.png';
-  static final matchScoreDetailsHeader = 'assets/images/scores_header.png';
-  static final cylinder = 'assets/images/cylinder.jpeg';
+  static const topMatchCard = 'assets/images/top_matches_card.png';
+  static const matchScoreDetailsHeader = 'assets/images/scores_header.png';
 
-  static final imgMoveIn = 'assets/images/move_in.png';
-  static final imgAgree = 'assets/images/agree.png';
-
-  // Login Page
-  static final loginImg = 'assets/images/login.svg';
-
-  // SignUp Page
-  static final signUpImg = 'assets/images/signup.png';
-
-  // HomeNav Page
-  static final property = 'assets/images/logo.png';
-  static final construction = 'assets/images/construction.png';
-  static final propPlaceholder = 'assets/images/prop_placeholder.png';
-
-  //Profile Page
-  static final profile = 'assets/images/profile.png';
-
-  //AllApplicationsPage
-  static final buyHouse = 'assets/images/buy_house.png';
-
-  //PaymentsPage
-  static final transactions = 'assets/images/transactions.png';
 
   static const String rNadal= 'https://www.tennisworldusa.org/world_cms/images/tennis_players/player105.png';
   static const String dMedvedev= 'https://i.goalzz.com/?i=o%2Fn%2F6%2F14%2Fdaniil-medvedev-1.png';
